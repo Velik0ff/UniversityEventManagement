70c1d880167f819f2f7403e67f1ce1d1ad6d2189
Lyuboslav Velikov <luboslav.velikov@gmail.com>
2019-11-09 23:34:03 +0000

    init project
    init project

---
+++ functions/connection.js
@@ -0,0 +1,22 @@
+const mongoose = require('mongoose');
+
+mongoose.Promise = global.Promise;
+
+mongoose.set('useCreateIndex', true);
+
+mongoose.connect('mongodb://localhost/event_system',{
+	auth: {
+		user: 'admin',
+		password: 'Uni@Liverpool@2019'
+	},
+	useNewUrlParser: true
+}); // connect to db
+
+
+mongoose.connection.once('open',function(){
+	console.log("Connection to DB established!");
+}).on('error',function(error){
+	console.log('Error: '+error);
+});
+
+module.exports = mongoose;
